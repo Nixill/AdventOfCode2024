782d4803a9cd185e71a4a4053772b9c2baf8772b
Steven <github@steven-h.net>
2024-12-03 12:47:59 -0500

    Day 3 part 1 attempt 1

---
+++ AoC2024/src/Days/Day3.cs
@@ -0,0 +1,41 @@
+using System.Text.RegularExpressions;
+using Nixill.Collections;
+
+namespace Nixill.AdventOfCode;
+
+public class Day3 : AdventDay
+{
+  static Regex FunctionParser = new(@"([a-z_]+)\((\d+(?:,\d+)*)\)", RegexOptions.IgnoreCase);
+
+  IDictionary<string, List<List<int>>> Functions = new EmptyConstructorGenerator<string, List<List<int>>>().Wrap();
+
+  public override void Run()
+  {
+    string input = InputStream.GetEverything();
+
+    foreach (Match mtc in FunctionParser.Matches(input))
+    {
+      if ((mtc.Groups[2].Value ?? "") == "")
+      {
+        Functions[mtc.Groups[1].Value].Add([]);
+      }
+      else
+      {
+        Functions[mtc.Groups[1].Value].Add(mtc.Groups[2].Value.Split(",").Select(int.Parse).ToList());
+      }
+    }
+
+    int answer = 0;
+
+    // Part 1 wants all the muls with two parameters.
+    foreach (List<int> paramList in Functions["mul"])
+    {
+      if (paramList.Count == 2)
+      {
+        answer += paramList[0] * paramList[1];
+      }
+    }
+
+    Part1Answer = answer.ToString();
+  }
+}
