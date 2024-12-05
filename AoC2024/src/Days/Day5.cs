12ab36bf8a8c490a841d5253a95b82e438e28d09
Steven <github@steven-h.net>
2024-12-05 10:06:43 -0500

    Day 5 part 1 attempt 1

---
+++ AoC2024/src/Days/Day5.cs
@@ -0,0 +1,49 @@
+using Nixill.Utils.Extensions;
+
+namespace Nixill.AdventOfCode;
+
+public class Day5 : AdventDay
+{
+  Dictionary<(int, int), int> SortingRules = [];
+
+  void AddSortingRule(int first, int second)
+  {
+    if (first < second) SortingRules[(first, second)] = -1;
+    else if (second < first) SortingRules[(second, first)] = 1;
+    else throw new ArgumentOutOfRangeException("The two arguments cannot be equal.");
+  }
+
+  int GetSortingRule(int first, int second)
+  {
+    if (first < second) return SortingRules.GetValueOrDefault((first, second), 0);
+    else if (second < first) return -SortingRules.GetValueOrDefault((second, first), 0);
+    else return 0;
+  }
+
+  bool IsCorrectlySorted(int first, int second)
+  {
+    return GetSortingRule(first, second) <= 0;
+  }
+
+  public override void Run()
+  {
+    foreach ((int first, int second) in InputStream
+      .GetLinesOfChunk()
+      .Select(s => s.Split("|").Select(int.Parse))
+      .Select(e => (e.First(), e.Last())))
+    {
+      AddSortingRule(first, second);
+    }
+
+    int answer1 = 0;
+
+    foreach (IEnumerable<int> ints in InputStream.GetLinesOfChunk()
+      .Select(s => s.Split(",").Select(int.Parse)))
+    {
+      if (ints.Pairs().All(p => IsCorrectlySorted(p.Item1, p.Item2)))
+        answer1 += ints.Middle(true);
+    }
+
+    Part1Answer = answer1.ToString();
+  }
+}
