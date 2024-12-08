e164a504e1ab298116abc7079cc482b3ca59c18b
Steven <github@steven-h.net>
2024-12-08 01:08:59 -0500

    Day 6 part 2 attempt 4

--- AoC2024/src/Days/Day6B.cs
+++ AoC2024/src/Days/Day6B.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices.Marshalling;
 using Nixill.Utils.Extensions;
 
 namespace Nixill.AdventOfCode;
@@ -32,22 +33,34 @@ public class Day6 : AdventDay
 
     while (true)
     {
-      D6PosDir next = Guard.Forward();
+      D6PosDir next = Guard.Forward;
 
-      while (Obstacles.Contains(next.Position))
+      if (Obstacles.Contains(next.Position))
       {
-        Guard = Guard.TurnRight();
-        next = Guard.Forward();
+        Guard = Guard.TurnRight;
+      }
+      else
+      {
+        if (next.X == Width || next.X == -1 || next.Y == Height || next.Y == -1) break;
+
+        Guard = next;
+        VisitedPositions.Add(Guard.Position);
       }
 
-      if (next.X == Width || next.X == -1 || next.Y == Height || next.Y == -1) break;
-
-      Guard = next;
       VisitedPosDirs.Add(Guard);
-      VisitedPositions.Add(Guard.Position);
-
-      if (VisitedPosDirs.Contains(Guard.TurnRight()) && !VisitedPositions.Contains(Guard.Forward().Position))
+      if (VisitedPosDirs.Contains(Guard.TurnRight) && !VisitedPositions.Contains(Guard.Forward.Position))
         answer2 += 1;
+
+      if (Obstacles.Contains(Guard.PosToLeft))
+      {
+        D6PosDir virtualGuard = Guard.TurnLeft;
+        while (!Obstacles.Contains(virtualGuard.Position) &&
+          !(virtualGuard.X == Width || virtualGuard.X == -1 || virtualGuard.Y == Height || virtualGuard.Y == -1))
+        {
+          VisitedPosDirs.Add(virtualGuard);
+          virtualGuard = virtualGuard.Backward;
+        }
+      }
     }
 
     Part1Answer = VisitedPositions.Count().ToString();
@@ -74,9 +87,9 @@ public readonly struct D6PosDir
   public static implicit operator D6PosDir((IntVector2 Position, IntVector2 Direction) input)
     => new(input.Position, input.Direction);
 
-  public D6PosDir Forward()
-    => new(Position + Direction, Direction);
-
-  public D6PosDir TurnRight()
-    => new(Position, Direction.RotateRight());
+  public D6PosDir Forward => new(Position + Direction, Direction);
+  public D6PosDir TurnRight => new(Position, Direction.RotateRight());
+  public D6PosDir Backward => new(Position - Direction, Direction);
+  public D6PosDir TurnLeft => new(Position, Direction.RotateLeft());
+  public IntVector2 PosToLeft => Position + Direction.RotateLeft();
 }
