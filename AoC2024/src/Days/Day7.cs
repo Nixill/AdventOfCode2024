9eb2adc74d5d229c10ba583fa2622f3c933e1605
Steven <github@steven-h.net>
2024-12-08 22:52:46 -0500

    Day 7 part 1 attempt 1

---
+++ AoC2024/src/Days/Day7.cs
@@ -0,0 +1,30 @@
+namespace Nixill.AdventOfCode;
+
+public class Day7 : AdventDay
+{
+  public override void Run()
+  {
+    foreach (string line in InputStream.GetLines())
+    {
+      long answer = long.Parse(line[..(line.IndexOf(':') - 1)]);
+      long[] numbers = line.Split(' ').Skip(1).Select(long.Parse).ToArray();
+
+      if (IsEquationSolvable(numbers, answer)) Part1Number += answer;
+    }
+  }
+
+  public static bool IsEquationSolvable(long[] longs, long target)
+  {
+    if (longs.Length == 0) return false;
+    if (longs.Length == 1) return longs[0] == target;
+
+    long lastLong = longs[^1];
+
+    bool solvable = false;
+    (long div, long rem) = Math.DivRem(target, lastLong);
+    if (rem == 0) solvable |= IsEquationSolvable(longs[..^1], div);
+    if (target >= lastLong) solvable |= IsEquationSolvable(longs[..^1], target - lastLong);
+
+    return solvable;
+  }
+}
