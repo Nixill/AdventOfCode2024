4988e1d25b3c39e4c52fdd2c1e814327c0a0c098
Steven <github@steven-h.net>
2024-12-09 13:42:41 -0500

    Day 9 part 2 attempt 1

--- AoC2024/src/Days/Day9.cs
+++ AoC2024/src/Days/Day9.cs
@@ -1,3 +1,4 @@
+using Nixill.Collections;
 using Nixill.Utils;
 using Nixill.Utils.Extensions;
 
@@ -7,8 +8,10 @@ public class Day9 : AdventDay
 {
   public override void Run()
   {
-    List<int> blocks = InputStream
-      .GetEverything()
+    string input = InputStream.GetEverything();
+
+    #region Part 1
+    List<int> blocksP1 = input
       .Select(c => (int)(c - '0'))
       .Chunk(2)
       .WithIndex()
@@ -17,24 +20,90 @@ public class Day9 : AdventDay
       .ToList();
 
     int i = 0;
-    int l = blocks.Count - 1;
+    int l = blocksP1.Count - 1;
 
     while (i <= l)
     {
-      if (blocks[i] == -1)
+      if (blocksP1[i] == -1)
       {
-        while (blocks[l] == -1)
+        while (blocksP1[l] == -1)
         {
-          blocks.RemoveAt(l--);
+          blocksP1.RemoveAt(l--);
           if (l < i) return;
         }
 
-        blocks[i] = blocks[l];
-        blocks.RemoveAt(l--);
+        blocksP1[i] = blocksP1[l];
+        blocksP1.RemoveAt(l--);
       }
 
-      Part1Number += blocks[i] * i;
+      Part1Number += blocksP1[i] * i;
       i++;
     }
+    #endregion
+
+    #region Part 2
+    AVLTreeDictionary<int, int> blocksP2 = [];
+    int cursor = 0;
+
+    foreach (var pair in input
+      .Select(c => (int)(c - '0'))
+      .Chunk(2)
+      .WithIndex())
+    {
+      blocksP2[cursor] = pair.Index;
+      cursor += pair.Item[0];
+      blocksP2[cursor] = -1;
+      cursor += pair.Item.ElementAtOrDefault(1);
+    }
+
+    while (cursor > 0)
+    {
+      (cursor, int fileSize, int fileID) = LowerFile(blocksP2, cursor);
+      int freeSpace = LowestFreeSpace(blocksP2, fileSize, cursor);
+
+      if (freeSpace < cursor)
+      {
+        blocksP2.Remove(cursor);
+        if (blocksP2.LowerEntry(cursor).Value != -1) blocksP2[cursor] = -1;
+        if (blocksP2.HigherEntry(cursor).AssignTo(out var higherEntry).Value == -1) blocksP2.Remove(higherEntry.Key);
+
+        blocksP2[freeSpace] = fileID;
+        if (!blocksP2.ContainsKey(freeSpace + fileSize)) blocksP2[freeSpace + fileSize] = -1;
+      }
+    }
+
+    foreach (var pair in blocksP2.Pairs())
+    {
+      if (pair.Item1.Value != -1)
+        Part2Number += pair.Item1.Value * Enumerable.Range(pair.Item1.Key, pair.Item2.Key - pair.Item1.Key).Sum();
+    }
+    #endregion
+  }
+
+  (int Start, int Size, int ID) LowerFile(AVLTreeDictionary<int, int> blocks, int from)
+  {
+    var lowerEntry = blocks.LowerEntry(from);
+    while (lowerEntry.Value == -1)
+    {
+      from = lowerEntry.Key;
+      lowerEntry = blocks.LowerEntry(from);
+    }
+
+    return (lowerEntry.Key, from - lowerEntry.Key, lowerEntry.Value);
+  }
+
+  int LowestFreeSpace(AVLTreeDictionary<int, int> blocks, int size, int maxIndex)
+  {
+    int start = 0;
+
+    while (start < maxIndex)
+    {
+      int higherKey = blocks.HigherKey(start);
+      if (blocks[start] == -1 && higherKey - start >= size)
+        return start;
+      start = higherKey;
+    }
+
+    return maxIndex;
   }
 }
