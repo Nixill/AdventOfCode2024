bf467dbb7624f97dbeb8a90df6c820f314b505da
Steven <github@steven-h.net>
2024-12-10 12:53:18 -0500

    Day 10 part 1 debug 1

--- AoC2024/src/Days/Day10.cs
+++ AoC2024/src/Days/Day10.cs
@@ -37,4 +37,22 @@ public class Day10 : AdventDay
       }
     }
   }
+
+  // debug functions!
+  public void GetTopology() => PrettyFormatGrid(Topology);
+  public void GetTrails() => PrettyFormatGrid(Trails);
+
+  public void PrettyFormatGrid(Grid<int> ints)
+  {
+    int largest = ints.Flatten().Max(t => t.Item);
+    int largestMagnitude = 1;
+    for (; largest > 0; largest /= 10)
+    {
+      largestMagnitude += 1;
+    }
+
+    if (largestMagnitude < 2) largestMagnitude = 2;
+
+    Console.WriteLine(string.Join('\n', ints.Select(r => string.Join(' ', r.Select(i => string.Format($"{{0,{largestMagnitude}}}", i))))));
+  }
 }
