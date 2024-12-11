c2dfc8d51d81281767f7627cc213f51a6db1f8b0
Nixill <github@nixill.net>
2024-12-11 12:17:10 -0500

    Day 11 part 1 attempt 1

---
+++ AoC2024/src/Days/Day11.cs
@@ -0,0 +1,36 @@
+using Nixill.Collections;
+using Nixill.Utils;
+using Nixill.Utils.Extensions;
+
+namespace Nixill.AdventOfCode;
+
+public class Day11 : AdventDay
+{
+  static AVLTreeDictionary<ulong, int> Magnitudes =
+    [.. Sequence.For((ulong)10, i => i % 10 == 0, i => i * 10)
+      .WithIndex()
+      .Prepend((Item: (ulong)0, Index: -1))
+      .Select(t => new KeyValuePair<ulong,int>(t.Item, t.Index + 2))];
+  static AVLTreeDictionary<int, ulong> ReverseMagnitudes =
+    [.. Magnitudes
+      .Select(kvp => new KeyValuePair<int, ulong>(int.Max(kvp.Value, 1), kvp.Key))];
+
+  static int GetMagnitude(ulong input) => Magnitudes.FloorEntry(input).Value;
+  static ulong GetNumberWithMagnitude(int input) => ReverseMagnitudes[input];
+
+  public override void Run()
+  {
+    IEnumerable<ulong> stones = InputStream.GetEverything().Split(' ').Select(ulong.Parse);
+
+    foreach (int i in Enumerable.Range(1, 25))
+    {
+      stones = stones.SelectMany<ulong, ulong>(n =>
+        n == 0 ? [1] :
+        GetMagnitude(n).AssignTo(out int mag) % 2 == 0 ? [Math.DivRem(n, GetNumberWithMagnitude(mag / 2)).AssignTo(out (ulong Quotient, ulong Remainder) divRem).Quotient, divRem.Remainder] :
+        [n * 2048]
+      );
+    }
+
+    Part1Number = stones.Count();
+  }
+}
