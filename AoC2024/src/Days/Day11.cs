627fc692e108595e231cdbce34e639099263aefb
Nixill <github@nixill.net>
2024-12-12 10:05:36 -0500

    Day 11 part 2 attempt 3

--- AoC2024/src/Days/Day11.cs
+++ AoC2024/src/Days/Day11.cs
@@ -14,47 +14,35 @@ public class Day11 : AdventDay
   static AVLTreeDictionary<int, ulong> ReverseMagnitudes =
     [.. Magnitudes
       .Select(kvp => new KeyValuePair<int, ulong>(kvp.Value, ulong.Max(kvp.Key, 1)))];
+  static Dictionary<(ulong, int), ulong> StoneCache = [];
 
   static int GetMagnitude(ulong input) => Magnitudes.FloorEntry(input).Value;
   static ulong GetNumberWithMagnitude(int input) => ReverseMagnitudes[input];
 
   public override void Run()
   {
-    int runCount = int.Parse(InputStream.ReadLine()!);
-
     ulong[] stones = InputStream.GetEverything().Split(' ').Select(ulong.Parse).ToArray();
 
-    foreach (int i in Enumerable.Range(1, runCount))
-    {
-      stones = stones.SelectMany<ulong, ulong>(ProcessStone).ToArray();
-      Console.WriteLine($"Ran {i} times, have {stones.Length} stones...");
-      if (i == 25) Part1Number = stones.Length;
-      if (i == 75) Part2Number = stones.Length;
-    }
+    Part1Number = (long)stones.Select(s => RecursiveCountStones(s, 25)).Sum();
+    Part2Number = (long)stones.Select(s => RecursiveCountStones(s, 75)).Sum();
   }
 
-  static IEnumerable<ulong> ProcessStone(ulong input)
-  {
-    if (input == 0)
+  static ulong RecursiveCountStones(ulong start, int iterations)
+    => StoneCache.GetOrSet((start, iterations), () =>
     {
-      yield return 1;
-      yield break;
-    }
-    else
-    {
-      int magnitude = GetMagnitude(input);
-      if (magnitude % 2 == 0)
+      if (iterations == 0) return 1;
+      if (start == 0) return RecursiveCountStones(1, iterations - 1);
+
+      int mag = GetMagnitude(start);
+
+      if (mag % 2 == 0)
       {
-        var divRem = Math.DivRem(input, GetNumberWithMagnitude(magnitude / 2 + 1));
-        yield return divRem.Quotient;
-        yield return divRem.Remainder;
-        yield break;
+        var divRem = Math.DivRem(start, GetNumberWithMagnitude((mag / 2) + 1));
+
+        return RecursiveCountStones(divRem.Quotient, iterations - 1)
+          + RecursiveCountStones(divRem.Remainder, iterations - 1);
       }
-      else
-      {
-        yield return input * 2024;
-        yield break;
-      }
-    }
-  }
+
+      return RecursiveCountStones(start * 2024, iterations - 1);
+    });
 }
