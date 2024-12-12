ce147e217ed67535d6b47bebe9aa0b2d1a5bbf21
Nixill <github@nixill.net>
2024-12-12 13:03:50 -0500

    Day 12 part 2 attempt 1

--- AoC2024/src/Days/Day12.cs
+++ AoC2024/src/Days/Day12.cs
@@ -13,6 +13,7 @@ public class Day12 : AdventDay
 
     Dictionary<int, int> areas = [];
     Dictionary<int, int> perimeters = [];
+    Dictionary<int, int> sides = [];
 
     // Construct region map and get areas
     foreach ((char chr, GridReference rfc) in plots.Flatten())
@@ -22,6 +23,7 @@ public class Day12 : AdventDay
         int regionNumber = nextRegionNumber++;
         areas[regionNumber] = 0;
         perimeters[regionNumber] = 0;
+        sides[regionNumber] = 0;
 
         foreach ((char chr2, GridReference rfc2) in plots.OrthogonalFloodSelect(rfc, (c, r) => c == chr))
         {
@@ -31,35 +33,90 @@ public class Day12 : AdventDay
       }
     }
 
-    // Now get perimeters
-    foreach ((int reg1, GridReference rfc1) in regions.Flatten())
-    {
-      foreach ((int reg2, GridReference rfc2) in regions.NearbyCells(rfc1, [(1, 0), (0, 1)]))
-      {
-        if (reg1 != reg2)
-        {
-          perimeters[reg1] += 1;
-          perimeters[reg2] += 1;
-        }
-      }
-    }
+    // Now get perimeters and sides
+    GetInternalEdges(regions, perimeters, sides, false);
+
+    int lastReg1 = -1, lastReg2 = -1;
 
     int maxX = regions.Width - 1;
     int maxY = regions.Height - 1;
 
     for (int x = 0; x <= maxX; x++)
     {
-      perimeters[regions[GridReference.XY(x, 0)]] += 1;
-      perimeters[regions[GridReference.XY(x, maxY)]] += 1;
+      int reg1 = regions[GridReference.XY(x, 0)];
+      perimeters[reg1] += 1;
+      if (reg1 != lastReg1)
+      {
+        sides[reg1] += 1;
+        lastReg1 = reg1;
+      }
+
+      int reg2 = regions[GridReference.XY(x, maxY)];
+      perimeters[reg2] += 1;
+      if (reg2 != lastReg2)
+      {
+        sides[reg2] += 1;
+        lastReg2 = reg2;
+      }
     }
+
+    lastReg1 = -1; lastReg2 = -1;
 
     for (int y = 0; y <= maxY; y++)
     {
-      perimeters[regions[GridReference.XY(0, y)]] += 1;
-      perimeters[regions[GridReference.XY(maxX, y)]] += 1;
+      int reg1 = regions[GridReference.XY(0, y)];
+      perimeters[reg1] += 1;
+      if (reg1 != lastReg1)
+      {
+        sides[reg1] += 1;
+        lastReg1 = reg1;
+      }
+
+      int reg2 = regions[GridReference.XY(maxX, y)];
+      perimeters[reg2] += 1;
+      if (reg2 != lastReg2)
+      {
+        sides[reg2] += 1;
+        lastReg2 = reg2;
+      }
     }
 
     // and get answer
     Part1Number = areas.Keys.Select(k => (long)perimeters[k] * areas[k]).Sum();
+    Part2Number = areas.Keys.Select(k => (long)sides[k] * areas[k]).Sum();
+  }
+
+  private static void GetInternalEdges(Grid<int> regions, Dictionary<int, int> perimeters, Dictionary<int, int> sides, bool transposed)
+  {
+    (int lastReg1, int lastReg2) = (-1, -1);
+    int lastCoord = -1;
+
+    foreach ((int reg1, GridReference rfc1) in (transposed ? regions.FlattenTransposed() : regions.Flatten()))
+    {
+      if (lastCoord != (transposed ? rfc1.Column : rfc1.Row))
+      {
+        lastReg1 = -1;
+        lastReg2 = -1;
+        lastCoord = (transposed ? rfc1.Column : rfc1.Row);
+      }
+
+      foreach ((int reg2, GridReference rfc2) in regions.NearbyCells(rfc1, [(transposed ? (1, 0) : (0, 1))]))
+      {
+        perimeters[reg1] += 1;
+        perimeters[reg2] += 1;
+
+        if (lastReg1 != reg1)
+        {
+          sides[reg1] += 1;
+          lastReg1 = reg1;
+        }
+
+        if (lastReg2 != reg2)
+        {
+          sides[reg2] += 1;
+          lastReg2 = reg2;
+        }
+      }
+    }
   }
 }
