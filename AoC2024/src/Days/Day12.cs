af92947cabc6bc626dc4ded609ab048a3d6c5808
Nixill <github@nixill.net>
2024-12-12 12:39:29 -0500

    Day 12 part 1 attempt 1

--- AoC2024/src/Days/Day12.cs
+++ AoC2024/src/Days/Day12.cs
@@ -31,6 +31,35 @@ public class Day12 : AdventDay
       }
     }
 
-    Console.WriteLine($"There are {nextRegionNumber - 1} regions.");
+    // Now get perimeters
+    foreach ((int reg1, GridReference rfc1) in regions.Flatten())
+    {
+      foreach ((int reg2, GridReference rfc2) in regions.NearbyCells(rfc1, [(1, 0), (0, 1)]))
+      {
+        if (reg1 != reg2)
+        {
+          perimeters[reg1] += 1;
+          perimeters[reg2] += 1;
+        }
+      }
+    }
+
+    int maxX = regions.Width - 1;
+    int maxY = regions.Height - 1;
+
+    for (int x = 0; x <= maxX; x++)
+    {
+      perimeters[regions[GridReference.XY(x, 0)]] += 1;
+      perimeters[regions[GridReference.XY(x, maxY)]] += 1;
+    }
+
+    for (int y = 0; y <= maxY; y++)
+    {
+      perimeters[regions[GridReference.XY(0, y)]] += 1;
+      perimeters[regions[GridReference.XY(maxX, y)]] += 1;
+    }
+
+    // and get answer
+    Part1Number = areas.Keys.Select(k => (long)perimeters[k] * areas[k]).Sum();
   }
 }
