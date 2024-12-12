9b7aa6c31836e27c09d59684b55ac5af78abf7f1
Nixill <github@nixill.net>
2024-12-11 19:55:10 -0500

    Day 11 part 1 attempt 4

--- AoC2024/src/Days/Day11.cs
+++ AoC2024/src/Days/Day11.cs
@@ -24,13 +24,34 @@ public class Day11 : AdventDay
 
     foreach (int i in Enumerable.Range(1, 25))
     {
-      stones = stones.SelectMany<ulong, ulong>(n =>
-        n == 0 ? [1] :
-        GetMagnitude(n).AssignTo(out int mag) % 2 == 0 ? [Math.DivRem(n, GetNumberWithMagnitude(mag / 2)).AssignTo(out (ulong Quotient, ulong Remainder) divRem).Quotient, divRem.Remainder] :
-        [n * 2048]
-      ).ToArray();
+      stones = stones.SelectMany<ulong, ulong>(ProcessStone).ToArray();
     }
 
     Part1Number = stones.Count();
+  }
+
+  static IEnumerable<ulong> ProcessStone(ulong input)
+  {
+    if (input == 0)
+    {
+      yield return 1;
+      yield break;
+    }
+    else
+    {
+      int magnitude = GetMagnitude(input);
+      if (magnitude % 2 == 0)
+      {
+        var divRem = Math.DivRem(input, GetNumberWithMagnitude(magnitude / 2 + 1));
+        yield return divRem.Quotient;
+        yield return divRem.Remainder;
+        yield break;
+      }
+      else
+      {
+        yield return input * 2024;
+        yield break;
+      }
+    }
   }
 }
