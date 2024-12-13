466735b7b99f980ee84f3677e8d6748b3f3cd5a3
Nixill <github@nixill.net>
2024-12-13 12:32:43 -0500

    Day 13 part 2 build 2

--- AoC2024/src/Days/Day13.cs
+++ AoC2024/src/Days/Day13.cs
@@ -14,13 +14,13 @@ public class Day13 : AdventDay
     foreach (string[] chunk in InputStream.GetChunksByLine().Select(c => c.ToArray()))
     {
       Match pressARegex = PushButtonText.Match(chunk[0]);
-      IntVector2 buttonAMove = (int.Parse(pressARegex.Groups[1].Value), int.Parse(pressARegex.Groups[2].Value));
+      LongVector2 buttonAMove = (int.Parse(pressARegex.Groups[1].Value), int.Parse(pressARegex.Groups[2].Value));
 
       Match pressBRegex = PushButtonText.Match(chunk[1]);
-      IntVector2 buttonBMove = (int.Parse(pressBRegex.Groups[1].Value), int.Parse(pressBRegex.Groups[2].Value));
+      LongVector2 buttonBMove = (int.Parse(pressBRegex.Groups[1].Value), int.Parse(pressBRegex.Groups[2].Value));
 
       Match prizeLocRegex = PrizeLocationText.Match(chunk[2]);
-      IntVector2 prizeLocation = (int.Parse(prizeLocRegex.Groups[1].Value), int.Parse(prizeLocRegex.Groups[2].Value));
+      LongVector2 prizeLocation = (int.Parse(prizeLocRegex.Groups[1].Value), int.Parse(prizeLocRegex.Groups[2].Value));
 
       Part1Number += Solve(buttonAMove, buttonBMove, prizeLocation);
       Part2Number += Solve(buttonAMove, buttonBMove, prizeLocation + Part2Modifier);
